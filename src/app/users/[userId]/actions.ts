92cc4de97398b99238e0b53dc58e336559dba9c2
viktoria yevpak <viktoriia.yevpak@cosmonova-broadcast.tv>
2025-02-20 15:51:11 +0200

    Finish the vido, all done

--- src/app/users/[userId]/actions.ts
+++ src/app/users/[userId]/actions.ts
@@ -4,11 +4,18 @@ import { updateUser } from "@/app/data-access/user";
 import { revalidatePath } from "next/cache";
 
 export async function updateNameAction (prevState:{
-    userId: string
+    userId: string,
+    name: string,
 }, formData: FormData) {
+    //sleep 1sec
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const userId = prevState.userId;
-        const newName = formData.get("name") as string;
-        await updateUser(userId, newName);
-        revalidatePath(`/users/${userId}`)
-    return {userId: userId}
+    const newName = formData.get("name") as string;
+    await updateUser(userId, newName);
+    revalidatePath(`/users/${userId}`);
+    return {
+        userId: userId,
+        name: "",
+        message: "succes"
+    }
 }
--- src/app/users/[userId]/form.tsx
+++ src/app/users/[userId]/form.tsx
@@ -3,19 +3,28 @@
 import {updateNameAction} from "./actions";
 import { useFormState} from "react-dom";
 import { SubmitButton } from "./button";
+import { useRef } from "react";
 
 export default function Form ({ userId } : {userId:string}){
+    const formRef = useRef<HTMLFormElement>(null);
     const [state, action] = useFormState(updateNameAction, {
         userId,
+        name: "",
+        message: '',
     })
+
+    if(state.message === "success"){
+        formRef.current?.reset();
+    }
 
     return (
                 <form
+                    ref={formRef}
                     className="flex gap-4"
                     action={action}
                 >
                     <input
-                        className="text-black border border-gray-400"
+                        className="text-black border border-gray-400 px-4 py-2 leading-tight focus:outline-none"
                         type="text" name="name"/>
 
                     <SubmitButton />
